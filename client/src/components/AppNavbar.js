b87dd3d8a300f54757bccb7031b61a3c66bffc6b
Sandy Wyper <sandywyper@gmail.com>
2020-04-21 14:33:40 +0100

    Removing redux dev tools

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -59,7 +59,7 @@ export class AppNavbar extends Component {
       <div>
         <Navbar color="dark" dark expand="sm" className="mb-5">
           <Container>
-            <NavbarBrand href="/">Shopping List</NavbarBrand>
+            <NavbarBrand>Shopping List</NavbarBrand>
             <NavbarToggler onClick={this.toggle} />
             <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
